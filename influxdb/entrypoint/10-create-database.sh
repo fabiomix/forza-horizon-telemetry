74c34ad9c032cf7f39233d2afaba2d01d967e796
fabiomix <fabiomix@users.noreply.github.com>
2022-11-25 16:17:40 +0100

    Telegraf tuning, Influx retention 24h, freeze containers version

--- influxdb/entrypoint/10-create-database.sh
+++ influxdb/entrypoint/10-create-database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-influx -execute 'CREATE DATABASE horizon'
-influx -execute 'CREATE RETENTION POLICY two_hours ON horizon DURATION 2h REPLICATION 1 DEFAULT';
+influx -execute 'CREATE DATABASE horizon';
+influx -execute 'CREATE RETENTION POLICY one_day ON horizon DURATION 24h REPLICATION 1 DEFAULT';
